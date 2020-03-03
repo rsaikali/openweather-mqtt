302e85dab9a3b01c124c7ba90cf85378b7f9ed08
Roland SAIKALI <roland@saikali.fr>
2020-03-03 18:42:14 +0100

    Update only if needed

--- openweather_mqtt.py
+++ openweather_mqtt.py
@@ -68,16 +68,17 @@ while True:
             for k, v in sorted(flatten_dict(data, delimiter='/').items()):
                 logger.info(f"{k:24} ---> {v}")
                 msgs[f"{MQTT_SERVICE_TOPIC}/{k}"] = v
+        else:
+            logger.info("No updated data from Openweather...")
 
-            # Publish openweather results on given MQTT broker every second, so we can view it often,
-            # but call Openweather API every ~1min (otherwise you'll get locked due to API rate limits)
-            last_update = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(data['dt']))
-
-            for i in range(60):
-                logger.info(f"Publishing to {MQTT_SERVICE_HOST}:{MQTT_SERVICE_PORT} [last_update={last_update}]")
-                for k, v in msgs.items():
-                    publish.single(topic=k, payload=str(v), hostname=MQTT_SERVICE_HOST, port=MQTT_SERVICE_PORT, client_id=MQTT_CLIENT_ID)
-                time.sleep(1)
+        # Publish openweather results on given MQTT broker every second, so we can view it often,
+        # but call Openweather API every ~1min (otherwise you'll get locked due to API rate limits)
+        last_update = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(data['dt']))
+        for i in range(60):
+            logger.info(f"Publishing to {MQTT_SERVICE_HOST}:{MQTT_SERVICE_PORT} [last_update={last_update}]")
+            for k, v in msgs.items():
+                publish.single(topic=k, payload=str(v), hostname=MQTT_SERVICE_HOST, port=MQTT_SERVICE_PORT, client_id=MQTT_CLIENT_ID)
+            time.sleep(1)
 
     except Exception:
         logger.error("An error occured:", exc_info=True)
